2ce610e3462b5fc2971d366f1b840f6df2e8f35c
niczinhoxx <nicolastiquinho007@gmail.com>
2026-08-21 13:05:09 -0300

    Base do Projeto

--- base.sql
+++ base.sql
@@ -4,12 +4,10 @@ CREATE TABLE clientes (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
+    telefone VARCHAR(20),
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-
--- 2. Tabela de Pacientes/Encomendas
 CREATE TABLE encomendas (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     cliente_id UUID NOT NULL REFERENCES clientes(id) ON DELETE CASCADE,
@@ -17,15 +15,12 @@ CREATE TABLE encomendas (
     status_atual VARCHAR(30) NOT NULL DEFAULT 'PENDENTE',
     origem VARCHAR(100) NOT NULL,
     destino VARCHAR(100) NOT NULL,
+    peso_kg DECIMAL(5,2) CHECK (peso_kg > 0),
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT chk_status CHECK (status_atual IN ('PENDENTE', 'EM_TRANSITO', 'SAIU_PARA_ENTREGA', 'ENTREGUE', 'CANCELADO'))
 );
 
-
-
-
--- 3. Tabela de Histórico de Status (Auditoria)
 CREATE TABLE historico_rastreamento (
     id SERIAL PRIMARY KEY,
     encomenda_id UUID NOT NULL REFERENCES encomendas(id) ON DELETE CASCADE,
